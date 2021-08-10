d1e8d96fb2d2c04ccca792890a553b73db1f695c
20011105 <20011105@LAPTOP-BM5I7NKK>
2021-08-10 10:20:02 +0800

    Initial version

---
+++ E66E-T1-CPA/src/MainMenu.java
@@ -0,0 +1,16 @@
+/*
+ * I declare that this code was written by me. 
+ * I will not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Matthew Ng Wei Chen 20011105, Aug 10, 2021 10:16:53 AM
+ */
+
+public class MainMenu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
