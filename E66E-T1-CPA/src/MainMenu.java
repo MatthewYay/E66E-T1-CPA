1eb760773d8c42847da35521671271e9a66c52f3
20011105 <20011105@LAPTOP-BM5I7NKK>
2021-08-13 16:30:17 +0800

    Matthew 13aug2021 4:30pm

--- E66E-T1-CPA/src/MainMenu.java
+++ E66E-T1-CPA/src/MainMenu.java
@@ -10,7 +10,40 @@ public class MainMenu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		SubjectManagement.start();
+		Helper.line(30, "-");
+		System.out.println("Welcome to Career Planning Application (CPA)!");
+		Helper.line(30, "-");
+		System.out.println("1. User account details");
+		System.out.println("2. Academic cluster details");
+		System.out.println("3. Career information details");
+		System.out.println("4. Subject details");
+		System.out.println("5. Prerequisites details");
+		System.out.println("6. Quit");
+		
+		int option = Helper.readInt("Choose an option: ");
+		while (option != 6) {
+			if (option == 1) {
+				//Patrick you key in your start method here (User account information)
+			}
+			else if (option == 2) {
+				//Philip you key in your start method here (academic cluster information)
+			}
+			else if (option == 3) {
+				//Hikma you key in your start method here (career information)
+			}
+			else if (option == 4) {
+				SubjectManagement.start();
+			}
+			else if (option == 5) {
+				PrerequisitesManagement.start();
+			}
+			else {
+				System.out.println("Invalid option!");
+			}
+		}
+		
+		System.out.println("Thank you for using Career Planning Application (CPA), goodbye!");
+		
 	}
 
 }
